220ffb86efaa957a2a7552a88e175e230b409a12
misaki-k <misamisa.chocolate@gmail.com>
2017-04-01 17:09:31 +0900

    Update app.js

--- frontend-nanodegree-arcade-game-master/js/app.js
+++ frontend-nanodegree-arcade-game-master/js/app.js
@@ -23,6 +23,7 @@ Enemy.prototype.update = function(dt) {
     //collide function needed
     this.height = 50;
     this.width = 80;
+    this.interval = setInterval(this, 5000);
   //var rect1 = {x: 5, y: 5, width: 50, height: 50}
   //var rect2 = {x: 20, y: 10, width: 10, height: 10}
 
